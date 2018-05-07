d6a0317133e38d3d80e4dcac41625005589151d0
uca <uca@uca.uca>
2018-05-07 12:16:03 -0600

    implemente View Pager Adapter

---
+++ app/src/main/java/com/uca/tablayout/PlanetViewPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.uca.tablayout;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by uca on 05-07-18.
+ */
+
+public class PlanetViewPagerAdapter extends FragmentPagerAdapter {
+    List<String> titleList;
+    List<Fragment> fragmentList;
+
+
+    public PlanetViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        titleList = new ArrayList<>();
+        fragmentList = new ArrayList<>();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void AddItem(String titleList, Fragment fragment){
+        this.titleList.add(titleList);
+        fragmentList.add(fragment);
+    }
+}
