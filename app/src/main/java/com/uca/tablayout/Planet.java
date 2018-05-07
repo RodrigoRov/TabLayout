0afd5c238c70302734783613bd0a066af5d41910
uca <uca@uca.uca>
2018-05-07 12:31:34 -0600

    se anadio framento inicial

---
+++ app/src/main/java/com/uca/tablayout/Planet.java
@@ -0,0 +1,8 @@
+package com.uca.tablayout;
+
+/**
+ * Created by uca on 05-07-18.
+ */
+
+class Planet {
+}
---
+++ app/src/main/java/com/uca/tablayout/PlanetsFragment.java
@@ -0,0 +1,19 @@
+package com.uca.tablayout;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * Created by uca on 05-07-18.
+ */
+
+public class PlanetsFragment extends Fragment {
+    RecyclerView recyclerView;
+    List<Planet> planetList;
+
+    public PlanetsFragment() {
+    }
+
+}
